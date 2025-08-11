3f98d10a8ab6def78faa3cbcbea306779990da10
jihoonjeong56 <jeong283056@gmail.com>
2025-08-11 11:50:50 +0900

    new file : starting springboot

---
+++ k5_iot_springboot/query.sql
@@ -0,0 +1,8 @@
+# 1. 스키마 생성(이미 존재하면 삭제)
+DROP DATABASE IF EXISTS k5_iot_springboot;
+# 2. 스키마 생성 + 문자셋/ 정렬 설정
+CREATE DATABASE IF NOT EXISTS k5_iot_springboot CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+# 3. 스키나 선택
+USE k5_iot_springboot;
+
+
