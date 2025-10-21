181f59d14d5b7e31a580c0196e111e7c31f34f51
jihoonjeong56 <jeong283056@gmail.com>
2025-10-21 16:31:53 +0900

    add notice the connecting front

--- k5_iot_springboot/query.sql
+++ k5_iot_springboot/query.sql
@@ -410,4 +410,14 @@ SELECT * FROM `orders`;
 SELECT * FROM `order_items`;
 SELECT * FROM `order_logs`;
 
+CREATE TABLE `notice`(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(100) NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+SELECT * FROM notice;
+
+
 USE k5_iot_springboot;
